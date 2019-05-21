d92b2318037455e8ba42eaea5bc9d044bb39bbf8
LukaszAlbinski <albinski.lukasz.mariusz@gmail.com>
2019-05-21 13:30:33 +0200

    pierwsze dodanie

---
+++ name.py
@@ -0,0 +1,10 @@
+name=input('Jak masz na imię?\n')
+age=int(input('Ile masz lat?\n')) # jak zadać to pytanie po sprawdzeniu imienia?
+if name=='Alicja':
+    print('Cześć, Alicja')
+if age<12:
+    print('nie jesteś Alicją, dzieciaku')
+if age>100:
+            print(' Nie jesteś Alicją dziadku')
+if age>2000:
+    print('W przeciwieństwie do Ciebie Alicja nie jest nieśmiertelna')
