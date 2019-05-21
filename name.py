769656d589c8f35fc85368ea7035695eef69230f
LukaszAlbinski <albinski.lukasz.mariusz@gmail.com>
2019-05-21 13:46:36 +0200

    no message

--- name.py
+++ name.py
@@ -1,10 +1,11 @@
 name=input('Jak masz na imię?\n')
-age=int(input('Ile masz lat?\n'))
 if name=='Alicja':
     print('Cześć, Alicja')
-if age<12:
-    print('nie jesteś Alicją, dzieciaku')
-if age>100:
-            print(' Nie jesteś Alicją dziadku')
-if age>2000:
-    print('W przeciwieństwie do Ciebie Alicja nie jest nieśmiertelna')
+else:
+    age=int(input('Ile masz lat?\n'))
+    if age<12:
+        print('Nie jesteś Alicją dzieciaku')
+    elif age>2000:
+        print('W przeciwności do Ciebie Alicja nie jest nieśmiertelna')
+    elif age>100:
+        print('Nie jesteś Alicją dziadku')
